2a4531b8baabd2fa6a15231afe9d15dee55f6c89
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 14:54:24 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    def initialize
+    end
+
+    def talk
+        puts "Hello World!"
+    end 
+
+    def walk
+        puts "The Person is walking"
+    end
+end 
+
+adele_goldberg = Person.new()
+alan_kay = Person.new()
